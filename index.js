2663892ea98fb14771adfb0affc8208cf74eca26
Steven Ford <sfordjasiri@gmail.com>
2019-11-25 18:24:27 +1300

    Attempting to change path.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("<PATH_TO_YOUR_APP>/web"))
+app.use(express.static("./web"))
 app.listen(process.env.PORT || 3007);
