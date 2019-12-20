7bc5df138a86d284dcc6434c41b31d475aedb4b9
Steven Ford <sfordjasiri@gmail.com>
2019-12-20 16:49:46 +1300

    Small changes to test if Heroku rebuilds it.

--- src/js/appController.js
+++ src/js/appController.js
@@ -81,7 +81,8 @@ define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojknockouttemplateutils',
       // Application Name used in Branding Area
       self.appName = ko.observable("App Name");
       // User Info used in Global Navigation area
-      self.userLogin = ko.observable("john.hancock@oracle.com");
+      //self.userLogin = ko.observable("john.hancock@oracle.com");
+      self.userLogin = ko.observable("steven.robert.ford@oracle.com");
 
       // Footer
       function footerLink(name, id, linkTarget) {
